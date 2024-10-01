e90516f7714a713665ed58a01121639d8500e9e0
Yurii <yurchik1997yurchik@gmail.com>
2024-10-02 00:14:04 +0300

    updated

---
+++ app/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+
+from app.config import get_settings
+
+settings = get_settings()
+
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    DATABASE_PARAMS = {}
+
+async_engine: AsyncEngine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
+
+async_session: async_sessionmaker = async_sessionmaker(
+    async_engine, expire_on_commit=False
+)
+
+
+class Base(DeclarativeBase):
+    """Базовый класс для декларативных определений классов."""
+
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+
+from app.kitten.router import router as kitten_router
+from app.breed.router import router as breed_router
+
+description = """REST API для администратора онлайн выставки котят"""
+
+
+app: FastAPI = FastAPI(
+    title="FastAPI breeds of kittens API",
+    description=description,
+    version="1.0.0",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Support Team",
+        "url": "http://example.com/contact/",
+        "email": "support@example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
+
+app.include_router(breed_router)
+app.include_router(kitten_router)
